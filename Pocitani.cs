b0a3385ee970b1778ba540def2c8467725c1d999
brenekv.03 <brenekv.03@A321-15>
2022-03-03 13:18:59 +0100

    Komenty

--- Pocitani.cs
+++ Pocitani.cs
@@ -8,5 +8,11 @@ namespace Metody09_14
 {
     class Pocitani
     {
+        public bool kvadratickaRovnice(int a, int b, int c, out int pocetKorenu)
+        {
+            //axna druhou + bx + c = 0
+            // D= bnadruhou
+            return;
+        }
     }
 }
